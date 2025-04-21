aeb9f0412673c59942284987254ac14f2ce6ccf0
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-21 10:25:28 +0900

    Alamofire 의존성 추가 및 패키지 설정 수정

--- HelloTuist/Project.swift
+++ HelloTuist/Project.swift
@@ -18,7 +18,7 @@ let project = Project(
             ),
             sources: ["HelloTuist/Sources/**"],
             resources: ["HelloTuist/Resources/**"],
-            dependencies: []
+            dependencies: [.package(product: "Alamofire"),]
         ),
         .target(
             name: "HelloTuistTests",
