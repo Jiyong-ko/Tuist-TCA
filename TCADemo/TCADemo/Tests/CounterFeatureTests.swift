c9c944a7b5f643a38b24dfb0065172df022b93d3
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-24 14:46:35 +0900

    테스트 추가: CounterFeature에 대한 기본 테스트 케이스 구현 및 불필요한 TCADemoTests 삭제

---
+++ TCADemo/TCADemo/Tests/CounterFeatureTests.swift
@@ -0,0 +1,27 @@
+//
+//  CounterFeatureTests.swift
+//  TCADemoTests
+//
+//  Created by NoelMacMini on 4/24/25.
+//
+@testable import TCADemo
+
+import ComposableArchitecture
+import Testing
+
+@MainActor
+struct CounterFeatureTests {
+    @Test
+    func basics() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.incrementButtonTapped) { state in
+            state.count = 1
+        }
+        await store.send(.decrementButtonTapped) {
+            $0.count = 0
+        }
+    }
+}
